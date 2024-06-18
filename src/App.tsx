a55443bed0769fbe5c22f05a72f1105385baf58b
Dan McAtee <danielmcatee@me.com>
2024-06-18 08:20:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  // Manage cart state
+  // Manage cart visibility
+
+  return (
+    <>
+      <header className="shadow-md py-4 px-6 flex justify-between">
+        <h1 className="text-xl font-bold">Wacky Wonders Emporium</h1>
+        {/* Cart Toggle */}
+      </header>
+      <div className="px-4 py-8">{/* Product List */}</div>
+    </>
+  );
+}
+
+export default App;
